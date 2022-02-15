611907dbbdb07226357bd8ae1db320558d15161d
SimplyV <gillian@mfmdigital.com>
2022-02-15 18:02:14 +0100

    Added possibility to add a categories

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,6 +9,12 @@ function openSideCategories(){
     x.style.display = "flex";
     x.classList.add('active');
   }
+  window.onkeydown = function( event ) {
+    if ( event.keyCode == 27 ) {
+        x.style.display = "none";
+        x.classList.remove('active');
+    }
+};
 }
 
 // Initialisation du DarkModeJS ( options )
@@ -30,3 +36,89 @@ const options = {
 const darkmode = new Darkmode(options);
 darkmode.showWidget();
 
+// Initiation ajout catégories
+
+var categories = ['Travail', 'Événement', 'Cours'];
+
+function addCategories(){
+
+  var categoriesInput = document.getElementById('categoriesInput');
+  var categoriesInputText = document.getElementById('categoriesInput').value;
+
+  if(categoriesInputText != ""){
+    categories.push(categoriesInputText);
+    var option = document.createElement('option');
+    option.text = categoriesInputText;
+    option.value = categoriesInputText;
+    selectCategories.add(option);
+    categoriesInput.value = "";
+  }
+}
+ // Ajout des catégories dans le select
+    var selectCategories = document.getElementById('slct');
+    for(i = 0; i < categories.length; i++){
+      var opt = document.createElement('option');
+      opt.value = categories[i];
+      opt.innerHTML = categories[i];
+      selectCategories.appendChild(opt);
+    }
+
+
+
+
+
+function addTask(){
+  var titleTask = document.getElementById('taskTitle').value;
+  var categoriesTask = document.getElementById("slct").options[document.getElementById('slct').selectedIndex].text;
+  var dateTask = document.getElementById("slctdate").options[document.getElementById('slctdate').selectedIndex].text;
+  var taskDesc = document.getElementById('taskDesc').value;
+
+
+
+  // alert(titleTask + ' ' + categoriesTask + ' ' + dateTask + ' ' + taskDesc);
+}
+
+jQuery(document).ready(function($){
+
+
+  // $('.what-container').each(function(){ 
+  //   var parent=$(this).parent()
+  //   var sliderContainer=document.createElement('div')
+  //   sliderContainer.classList.add('swiper');
+  //   var sliderWrapper = document.createElement('div');
+  //   sliderWrapper.classList.add('swiper-wrapper');
+
+  //   $(this).find('.what-block').each(function(){
+  //     var slide=document.createElement('div')
+  //     slide.classList.add('swiper-slide')
+  //     slide.innerHTML=$(this).html()
+  //     sliderWrapper.appendChild(slide)
+  //   })
+    
+  //   sliderContainer.appendChild(sliderWrapper)
+  //   var sliderPagination = document.createElement('div');
+  //   sliderPagination.classList.add('swiper-pagination');
+  //   sliderContainer.appendChild(sliderPagination)
+    
+  //   $(parent).append(sliderContainer)
+
+  //   const swiper = new Swiper('.swiper', {
+  //     // Optional parameters
+  //     direction: 'horizontal',
+  //     loop: true,
+  //     slidesPerView: 1,
+    
+  //     // If we need pagination
+  //     pagination: {
+  //       el: '.swiper-pagination',
+  //       clickable: true,
+  //     },
+    
+  
+  //   });
+    
+  // })
+  
+  
+});
+  
